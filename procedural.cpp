d29a0e25595356f897574b9e47d58b4f037c0cb9
Pratham-2669 <kubadepratham11@gmail.com>
2026-07-19 03:31:46 +0530

    updates

---
+++ procedural.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"code to find if a number is even or odd";
+    int n;
+    cout<<endl<<"enter n value";
+    cin>>n;
+    if(n%2==0)
+    cout<<endl<<"the number is even";
+    else
+    cout<<"the number is odd";
+}
