8fc13f29ec52fff324031bf74fc9517e61da0368
Pratham-2669 <kubadepratham11@gmail.com>
2026-08-18 11:00:50 +0530

    adding

---
+++ array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a[20];
+    int n;
+    
+    cout << "Enter the number of elements you want to use (max 20): ";
+    cin >> n;
+
+    
+    for(int i = 0; i < n; i++) 
+    {
+        cout << endl << "Enter value at position " << (i + 1) << ": ";
+        cin >> a[i];
+    }
+
+    
+    for(int i = 0; i < n; i++) 
+    {
+        cout << endl << "Value at position " <<i + 1<< " is: " << a[i];
+    }
+    
+    return 0;
+}
