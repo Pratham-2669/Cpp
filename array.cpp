47401e06f181cd7fc366b11cf3fff0324ae123eb
Pratham-2669 <kubadepratham11@gmail.com>
2026-08-26 06:16:34 +0530

    edited

--- array.cpp
+++ array.cpp
@@ -5,22 +5,16 @@ int main()
 {
     int a[20];
     int n;
-    
     cout << "Enter the number of elements you want to use (max 20): ";
     cin >> n;
-
-    
     for(int i = 0; i < n; i++) 
     {
         cout << endl << "Enter value at position " << (i + 1) << ": ";
         cin >> a[i];
     }
-
-    
     for(int i = 0; i < n; i++) 
     {
-        cout << endl << "Value at position " <<i + 1<< " is: " << a[i];
+        cout<<a[i];
     }
-    
     return 0;
 }
